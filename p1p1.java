67764308b836d9c5ad47a65bbec92e562f6224aa
VarunPithiya17 <varunpithiya41@gmail.com>
2022-09-27 13:06:38 +0530

    Create p1p1.java

---
+++ p1p1.java
@@ -0,0 +1,19 @@
+//BY 21CE109 VARUN PITHIYA
+/*QUESTION :
+Introduction to Object-Oriented Concepts, comparison of Java with other object-orientedprogramming languages. Introduction to JDK, JRE, JVM, Javadoc,
+command line argument */
+
+/*Answers :
+JDK : Java Development Kit (in short JDK) is a Kit which provides the environment to Develop and execute(run ) the Java program.
+
+JRE: Java Runtime Environment (to say JRE) is an installation package that provides environment to only run(not develop) the java program onto your machine.
+
+JVM: A Java virtual machine (JVM) is a virtual machine that enables a computer to run Java programs as well as programs written
+    in other languages and compiled to Java bytecode
+
+JavaDoc  Javadoc (originally cased JavaDoc) is a documentation generator created by Sun Microsystems for the Java language (now owned by Oracle Corporation)
+    for generating API documentation in HTML format from Java source code
+   
+Command Line Argument : The java command-line argument is an argument i.e. passed at the time of running the java program.
+   The arguments passed from the console can be received in the java program and ittheyan be used as an input  */
+
